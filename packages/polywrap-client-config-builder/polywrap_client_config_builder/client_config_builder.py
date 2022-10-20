adac433621693d96b0a9cc24d4fd3e3a5d5b0e09
DaoAdvocate <12145726+rihp@users.noreply.github.com>
2022-10-20 18:24:06 +0200

    adding types for clientconfigbuilder

--- packages/polywrap-client-config-builder/polywrap_client_config_builder/client_config_builder.py
+++ packages/polywrap-client-config-builder/polywrap_client_config_builder/client_config_builder.py
@@ -1,18 +1,16 @@
-from polywrap_core import Uri 
+from polywrap_core import Uri, ClientConfig
 from dataclasses import dataclass
 
 from polywrap_core.types.env import Env
 from typing import List
 
 from polywrap_core.types.uri_resolver import IUriResolver
+from polywrap_core.types.wrapper import WrapperCache
 
 # from "./bundles" import get_default_client_config
 
 # Replicate this file
 # https://github.com/polywrap/toolchain/blob/origin/packages/js/client-config-builder/src/ClientConfigBuilder.ts
-
-def just_a_function():
-    return "Yes"
 
 @dataclass(slots=True, kw_only=True)
 class ClientConfigBuilder():
@@ -23,21 +21,27 @@ class ClientConfigBuilder():
     # redirects
 
 
-    def add(self):
-        """
-        Appends each property of the supplied config object to the corresponding array of the builder's config.
-        """
-        pass
+def add(self, config: ClientConfigBuilder) -> ClientConfigBuilder:
+    """
+    Appends each property of the supplied config object to the corresponding array of the builder's config.
+    """
+    pass
 
-    def add_defaults(self):
-        """
-        Adds the defaultClientConfig object.
+def add_defaults(self, wrapper_cache: WrapperCache=None) -> ClientConfigBuilder:
+    """
+    Adds the defaultClientConfig object.
 
-        """
-        pass
+    """
+    pass
 
-    def build(self):
-        """
-        Returns a sanitized config object from the builder's config.
-        """
-        pass
+def add_env(self, uri: Uri | str, env ) -> ClientConfigBuilder:
+    pass
+
+def remove_env(self, uri: Uri | str ) -> ClientConfigBuilder:
+    pass
+
+def build(self):
+    """
+    Returns a sanitized config object from the builder's config.
+    """
+    pass
--- packages/polywrap-client-config-builder/tests/test_client_config_builder.py
+++ packages/polywrap-client-config-builder/tests/test_client_config_builder.py
@@ -1,9 +1,15 @@
-from polywrap_client_config_builder import ClientConfigBuilder, just_a_function
+from polywrap_client_config_builder import ClientConfigBuilder
+
 
 def test_client_config_builder_adds_default_config():
     ClientConfigBuilder.add_defaults()
     pass
 
+
+
 def test_client_config_builder_adds_config():
     config = ClientConfigBuilder().add({})
+    pass
+
+def test_client_config_builder_adds_uri_resolver():
     pass
