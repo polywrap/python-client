a030781d16ca487ea449b3905261c82dce8cb63b
DaoAdvocate <12145726+rihp@users.noreply.github.com>
2022-10-20 15:09:38 +0200

    cleanup bignumber tests file

--- packages/polywrap-client/tests/test_bignumber.py
+++ packages/polywrap-client/tests/test_bignumber.py
@@ -1,12 +1,13 @@
-from pathlib import Path
+# Polywrap Python Client - https://polywrap.io
+# BigNumber wrapper schema - https://wrappers.io/v/ipfs/Qme2YXThmsqtfpiUPHJUEzZSBiqX3woQxxdXbDJZvXrvAD
 
+from pathlib import Path
 from polywrap_client import PolywrapClient
 from polywrap_core import Uri, InvokerOptions
 
 
 async def test_invoke_bignumber_1arg_and_1prop():
     client = PolywrapClient()
-    # BigNumber wrapper schema - https://wrappers.io/v/ipfs/Qme2YXThmsqtfpiUPHJUEzZSBiqX3woQxxdXbDJZvXrvAD
     uri = Uri(f'fs/{Path(__file__).parent.joinpath("cases", "simple-invoke" ,"wrapperBigNumber.wasm").absolute()}')
     args = { "arg1": "123", # The base number
         "obj": {
@@ -15,12 +16,10 @@ async def test_invoke_bignumber_1arg_and_1prop():
     }
     options = InvokerOptions(uri=uri, method="method", args=args, encode_result=False)
     result = await client.invoke(options)
-    print(result)
     assert result.result == "123000"
 
 async def test_invoke_bignumber_with_1arg_and_2props():
     client = PolywrapClient()
-    # BigNumber wrapper schema - https://wrappers.io/v/ipfs/Qme2YXThmsqtfpiUPHJUEzZSBiqX3woQxxdXbDJZvXrvAD
     uri = Uri(f'fs/{Path(__file__).parent.joinpath("cases", "simple-invoke" ,"wrapperBigNumber.wasm").absolute()}')
     args = {
         "arg1": "123123",
@@ -31,12 +30,10 @@ async def test_invoke_bignumber_with_1arg_and_2props():
     }
     options = InvokerOptions(uri=uri, method="method", args=args, encode_result=False)
     result = await client.invoke(options)
-    print(result)
     assert result.result == str(123123*1000*4)
 
 async def test_invoke_bignumber_with_2args_and_1prop():
     client = PolywrapClient()
-    # BigNumber wrapper schema - https://wrappers.io/v/ipfs/Qme2YXThmsqtfpiUPHJUEzZSBiqX3woQxxdXbDJZvXrvAD
     uri = Uri(f'fs/{Path(__file__).parent.joinpath("cases", "simple-invoke" ,"wrapperBigNumber.wasm").absolute()}')
     args = {
         "arg1": "123123",
@@ -47,12 +44,10 @@ async def test_invoke_bignumber_with_2args_and_1prop():
     }
     options = InvokerOptions(uri=uri, method="method", args=args, encode_result=False)
     result = await client.invoke(options)
-    print(result)
     assert result.result == str(123123*1000*444)
 
 async def test_invoke_bignumber_with_2args_and_2props():
     client = PolywrapClient()
-    # BigNumber wrapper schema - https://wrappers.io/v/ipfs/Qme2YXThmsqtfpiUPHJUEzZSBiqX3woQxxdXbDJZvXrvAD
     uri = Uri(f'fs/{Path(__file__).parent.joinpath("cases", "simple-invoke" ,"wrapperBigNumber.wasm").absolute()}')
     args = {
         "arg1": "123123",
@@ -64,14 +59,11 @@ async def test_invoke_bignumber_with_2args_and_2props():
     }
     options = InvokerOptions(uri=uri, method="method", args=args, encode_result=False)
     result = await client.invoke(options)
-    print(result)
     assert result.result == str(123123*555*1000*4)
 
 async def test_invoke_bignumber_with_2args_and_2props_floats():
     client = PolywrapClient()
-    # BigNumber wrapper schema - https://wrappers.io/v/ipfs/Qme2YXThmsqtfpiUPHJUEzZSBiqX3woQxxdXbDJZvXrvAD
     uri = Uri(f'fs/{Path(__file__).parent.joinpath("cases", "simple-invoke" ,"wrapperBigNumber.wasm").absolute()}')
-    print(Uri)
     args = {
         "arg1": "123.123",
         "arg2": "55.5",
@@ -85,24 +77,4 @@ async def test_invoke_bignumber_with_2args_and_2props_floats():
     print(result)
     assert result.result == str(123.123*55.5*10.001*4)
 
-
-# async def test_invoke_bignumber_with_2args_and_2props_complex_numbers():
-#     client = PolywrapClient()
-#     # BigNumber wrapper schema - https://wrappers.io/v/ipfs/Qme2YXThmsqtfpiUPHJUEzZSBiqX3woQxxdXbDJZvXrvAD
-#     uri = Uri(f'fs/{Path(__file__).parent.joinpath("cases", "simple-invoke" ,"wrapperBigNumber.wasm").absolute()}')
-#     print(Uri)
-#     args = {
-#         # Question: Should bignumber work with complex numbers? 
-#         "arg1": "3j",
-#         "arg2": "5",
-#         "obj": {
-#             "prop1": "2",
-#            "prop2": "4"
-#         }
-#     }
-#     options = InvokerOptions(uri=uri, method="method", args=args, encode_result=False)
-#     result = await client.invoke(options)
-#     print(result)
-#     # Question: Should bignumber work with complex numbers? 
-#     assert result.result == str(3j*55*2*4)
 
