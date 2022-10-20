585e32fdd7fb23fc33a08599df9e08b5607a810b
DaoAdvocate <12145726+rihp@users.noreply.github.com>
2022-10-20 15:33:31 +0200

    feat: sha512 implemented

--- packages/polywrap-client/tests/test_sha3.py
+++ packages/polywrap-client/tests/test_sha3.py
@@ -1,19 +1,24 @@
 # Polywrap Python Client - https://polywrap.io
 # SHA3 Wrapper Schema - https://wrappers.io/v/ipfs/QmbYw6XfEmNdR3Uoa7u2U1WRqJEXbseiSoBNBt3yPFnKvi
 
+from mailbox import _mboxMMDFMessage
 from pathlib import Path
 from polywrap_client import PolywrapClient
 from polywrap_core import Uri, InvokerOptions
+import hashlib
+import sha3
 
 client = PolywrapClient()
 uri = Uri(f'fs/{Path(__file__).parent.joinpath("cases", "simple-invoke", "wrapperSHA3.wasm").absolute()}')
+
 args = {"message": "hello polywrap!"}
 
 async def test_invoke_sha3_512():
     options = InvokerOptions(uri=uri, method="sha3_512", args=args, encode_result=False)
     result = await client.invoke(options)
-    print(result)
-    assert result.result == False
+    s = hashlib.sha512()
+    s.update(b"hello polywrap!")
+    assert result.result == s.hexdigest()
 
 async def test_invoke_sha3_384():
     options = InvokerOptions(uri=uri, method="sha3_384", args=args, encode_result=False)
