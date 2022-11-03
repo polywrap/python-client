dd172a595fdc5b10094119b904636a2cc257822f
DaoAdvocate <12145726+rihp@users.noreply.github.com>
2022-11-03 13:58:55 +0100

    skipped failing tests

--- packages/polywrap-client/tests/test_bigint_type.py
+++ packages/polywrap-client/tests/test_bigint_type.py
@@ -6,7 +6,7 @@ from polywrap_client import PolywrapClient
 from polywrap_core import Uri, InvokerOptions
 import pytest
 
-#@pytest.mark.skip(reason="can't invoke bigint-rs wrapper due to an error related to wasmtime")
+@pytest.mark.skip(reason="can't invoke bigint-rs wrapper due to an error related to wasmtime")
 async def test_invoke_bigint_rs_with_1arg_and_1prop():
     client = PolywrapClient()
     uri = Uri(f'fs/{Path(__file__).parent.joinpath("cases", "bigint-type-rs").absolute()}')
