d4fa4adaa1846cd3dfe368753563608780716a01
DaoAdvocate <12145726+rihp@users.noreply.github.com>
2022-11-21 11:16:48 +0100

    cleanup

--- packages/polywrap-client-config-builder/tests/test_client_config_builder.py
+++ packages/polywrap-client-config-builder/tests/test_client_config_builder.py
@@ -20,7 +20,7 @@ env_uriY = Uri("wrap://ipfs/filecoin.wrapper.two")
 
 # ENVS 
 
-def test_client_config_builder_set_many_envs():
+def test_client_config_builder_set_env():
     ccb = ClientConfigBuilder()
     envs = { env_uriX: env_varA }
     ccb = ccb.set_env( env_varA, env_uriX)
@@ -43,38 +43,6 @@ def test_client_config_builder_add_env_updates_env_value():
     client_config: ClientConfig = ccb.build() # build a new client config object
     assert asdict(client_config) == asdict(ClientConfig(envs={env_uriX: env_varB}, interfaces={}, resolver = None, wrappers=[]))
 
-
-# def test_client_add_envs():
-#     client_config: ClientConfigBuilder = ClientConfigBuilder() # instantiate new client config builder
-#     for env in test_envs3: 
-#         print("adding env: ", env)
-#         client_config = client_config.add_env(env.uri, env.env)
-#     client: ClientConfig = client_config.build()
-#     assert client.get_envs() == {
-#         Uri("wrap://ens/eth.plugin.one") : { 'color': "red", 'size': "small" },
-#         Uri("wrap://ens/ipfs.plugin.two"): { 'dog': "terrier", 'cat': "persian" },
-#     }
-
-# def test_client_config_builder_set_env():
-#     client = ClientConfigBuilder() # instantiate new client
-#     client = client.add_env(env_varB)
-#     client.set_env(env_varB) # add basic envs to client
-#     print(client.config)
-#     assert client.config['envs'] == env_varB
-
-# @pytest.mark.skip("not yet implemented")
-# def test_client_config_builder_set_many_envs():
-#     # client = ClientConfigBuilder.build() # instantiate new client
-#     # for new_env in test_envs1: #add basic envs
-#     #     client = client.add_env(new_env.uri, new_env.env)
-#     # for new_env in test_envs2: # set the new envs, which should overwrite the old ones
-#     #     client = client.set_env(new_env)
-#     # assert client.config['envs'] == [
-#     #     Env(uri=Uri('wrap://ens/test.plugin.one'), env={'color': 'red', 'size': 'small'}), 
-#     #     Env(uri=Uri("wrap://ens/test.plugin.two"), env={'dog': 'poodle', 'cat': 'siamese'}), 
-#     #     Env(uri=Uri("wrap://ens/test.plugin.three"), env={'vehicle': 'bycicle', 'bird': 'parrot', 'fruit': 'apple'})]
-#     pass
-
 # INTERFACES AND IMPLEMENTATIONS
 
 def test_client_config_builder_adds_interface_implementations():
@@ -94,7 +62,6 @@ def test_client_config_builder_add_wrapper():
     client_config = ccb.build()
     assert asdict(client_config) == asdict(ClientConfig(envs={}, interfaces={}, resolver = None, wrappers=[wrapper]))
 
-
 def test_client_config_builder_adds_multiple_wrappers():
     ccb = ClientConfigBuilder()
     wrappers = [Uri("wrap://ens/uni.wrapper.eth"), Uri("wrap://ens/https.plugin.eth")]
@@ -113,7 +80,6 @@ def test_client_config_builder_removes_wrapper():
     assert asdict(client_config) == asdict(ClientConfig(envs={}, interfaces={}, resolver = None, wrappers=[]))
 
 # RESOLVER 
-
 
 @pytest.mark.skip("Should implement IURIResolver interface")
 def test_client_config_builder_set_uri_resolver():
