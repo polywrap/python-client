c27ff7483d52d91a8646df8e72ee450fa7ea230e
DaoAdvocate <12145726+rihp@users.noreply.github.com>
2022-11-24 11:48:10 +0100

    chore: refactoring abstract base classes to remove IClientConfigBuilder

--- packages/polywrap-client-config-builder/polywrap_client_config_builder/client_config_builder.py
+++ packages/polywrap-client-config-builder/polywrap_client_config_builder/client_config_builder.py
@@ -15,82 +15,35 @@ class ClientConfig():
     wrappers: List[Uri]
 
 
-class IClientConfigBuilder(ABC):
+class BaseClientConfigBuilder(ABC):
     """
-    Interface used by the BaseClientConfigBuilder class. 
-    This interface is used to configure the polywrap client before it executes a call
-    It defines the methods that can be used to configure the ClientConfig object.
+    An abstract base class of the `ClientConfigBuilder`, which uses the ABC module
+    to define the methods that can be used to configure the `ClientConfig` object.
     """
+
+    def __init__(self):
+        self.config = ClientConfig(envs={}, interfaces={}, resolver=[], wrappers= [])
 
     @abstractmethod
     def build(self) -> ClientConfig:
         """Returns a sanitized config object from the builder's config."""
         pass
 
-    def get_envs(self) -> Optional[Dict[Uri, Dict[str, Any]]]:
-        """Returns the envs dictionary from the builder's config."""
-        pass
-
     @abstractmethod
-    def set_env(self, env: Dict[str, Any], uri: Uri)-> ClientConfig:
-        pass
-
-    @abstractmethod
-    def add_env(self, env: Dict[str, Any], uri: Uri) -> ClientConfig:
-        pass
-
-    @abstractmethod
-    def add_envs(self, envs: List[Env], uri: Uri) -> ClientConfig:
-        pass
-
-    @abstractmethod
-    def add_interface_implementations(self, interface_uri: Uri, implementations_uris: List[Uri]):
-        pass
-
-    @abstractmethod
-    def add_wrapper(self, wrapper_uri: Uri) -> ClientConfig:
-        pass
-
-    @abstractmethod
-    def add_wrappers(self, wrapper_uris: Optional[List[Uri]]) -> ClientConfig:
-        pass
-
-    @abstractmethod
-    def set_resolver(self, resolver: IUriResolver) -> ClientConfig:
-        pass
-
-    @abstractmethod
-    def add_resolver(self, resolver: IUriResolver) -> ClientConfig:
-        pass
-
-    @abstractmethod
-    def add_resolvers(self, resolvers_list: List[IUriResolver]) -> ClientConfig:
-        pass
-class BaseClientConfigBuilder(IClientConfigBuilder):
-    """A concrete class of the Client Config Builder, which uses the IClientConfigBuilder Abstract Base Class"""
-    # config: ClientConfig
-
-    def __init__(self):
-        self.config = ClientConfig(envs={}, interfaces={}, resolver=[], wrappers= [])
-
-    def build(self)-> ClientConfig:
-        """
-        Returns a sanitized config object from the builder's config.
-        """
-        return self.config
-
     def get_envs(self)-> Dict[Uri, Dict[str, Any]]:
+        """Returns the envs dictionary from the builder's config."""
         return self.config.envs
-        
-    def set_env(self, env: Env={'test':'tested'}, uri: Uri=Uri("wrap://ens/eth.plugin.one"))-> IClientConfigBuilder:
-        # TODO:  This is a temporary solution to the problem of the Uri / Env not being able to pass as None initially 
-        # should be (self, env: Env=None, uri: Uri=None):  but that causes an error
+
+    @abstractmethod    
+    def set_env(self, env: Env, uri: Uri):
+        """Sets the envs dictionary in the builder's config, overiding any existing values."""
         if (env or uri) is None:
             raise KeyError("Must provide both an env or uri")
         self.config.envs[uri] = env
         return self
 
-    def add_env(self, env: Env = None , uri: Uri = None)-> IClientConfigBuilder:
+    @abstractmethod
+    def add_env(self, env: Env = None , uri: Uri = None):
         """Adds an environment (in the form of an `Env`) for a given uri, without overwriting existing environments,
         unless the env key already exists in the environment, then it will overwrite the existing value"""
         if (env or uri) is None:
@@ -103,7 +56,8 @@ class BaseClientConfigBuilder(IClientConfigBuilder):
             self.config.envs[uri] = env
         return self
 
-    def add_envs(self, envs: List[Env], uri: Uri = None) -> IClientConfigBuilder:
+    @abstractmethod
+    def add_envs(self, envs: List[Env], uri: Uri = None):
         """
         Adds a list of environments (each in the form of an `Env`) for a given uri
         """
@@ -111,7 +65,8 @@ class BaseClientConfigBuilder(IClientConfigBuilder):
             self.add_env(env, uri )
         return self
 
-    def add_interface_implementations(self, interface_uri: Uri, implementations_uris: List[Uri]) -> IClientConfigBuilder:
+    @abstractmethod
+    def add_interface_implementations(self, interface_uri: Uri, implementations_uris: List[Uri]):
         """
         Adds a list of implementations (each in the form of an `Uri`) for a given interface
         """
@@ -123,14 +78,16 @@ class BaseClientConfigBuilder(IClientConfigBuilder):
             self.config.interfaces[interface_uri] = implementations_uris
         return self
 
-    def add_wrapper(self, wrapper_uri: Uri) -> IClientConfigBuilder:
+    @abstractmethod
+    def add_wrapper(self, wrapper_uri: Uri) :
         """
         Adds a wrapper to the list of wrappers
         """
         self.config.wrappers.append(wrapper_uri)
         return self
 
-    def add_wrappers(self, wrappers_uris: List[Uri]) -> IClientConfigBuilder:
+    @abstractmethod
+    def add_wrappers(self, wrappers_uris: List[Uri]) :
         """
         Adds a list of wrappers to the list of wrappers
         """
@@ -138,21 +95,24 @@ class BaseClientConfigBuilder(IClientConfigBuilder):
             self.add_wrapper(wrapper_uri)
         return self
 
-    def remove_wrapper(self, wrapper_uri: Uri) -> IClientConfigBuilder:
+    @abstractmethod
+    def remove_wrapper(self, wrapper_uri: Uri) :
         """
         Removes a wrapper from the list of wrappers
         """
         self.config.wrappers.remove(wrapper_uri)
         return self
 
-    def set_resolver(self, uri_resolver) -> IClientConfigBuilder:
+    @abstractmethod
+    def set_resolver(self, uri_resolver):
         """
         Sets a single resolver for the `ClientConfig` object before it is built
         """
         self.config.resolver = [uri_resolver]
         return self
 
-    def add_resolver(self, resolver: IUriResolver) -> IClientConfigBuilder:
+    @abstractmethod
+    def add_resolver(self, resolver: IUriResolver) :
         """
         Adds a resolver to the list of resolvers
         """
@@ -162,7 +122,8 @@ class BaseClientConfigBuilder(IClientConfigBuilder):
         self.config.resolver.append(resolver)
         return self
 
-    def add_resolvers(self, resolvers_list: List[IUriResolver]) -> IClientConfigBuilder:
+    @abstractmethod
+    def add_resolvers(self, resolvers_list: List[IUriResolver]):
         """
         Adds a list of resolvers to the list of resolvers
         """
@@ -171,4 +132,54 @@ class BaseClientConfigBuilder(IClientConfigBuilder):
         return self
 
 class ClientConfigBuilder(BaseClientConfigBuilder):
-   ...
+   
+    def build(self)-> ClientConfig:
+        """
+        Returns a sanitized config object from the builder's config.
+        """
+        return self.config
+    
+    def get_envs(self) -> Dict[Uri, Dict[str, Any]]:
+        return super().get_envs()
+
+    def set_env(self, env: Env, uri: Uri)-> BaseClientConfigBuilder:
+        super().set_env(env, uri)
+        return self
+
+    def add_env(self, env: Env, uri: Uri)-> BaseClientConfigBuilder:
+        super().add_env(env, uri)
+        return self
+
+    def add_envs(self, envs: List[Env], uri: Uri)-> BaseClientConfigBuilder:
+        super().add_envs(envs, uri)
+        return self
+
+    def add_interface_implementations(self, interface_uri: Uri, implementations_uris: List[Uri])-> BaseClientConfigBuilder:
+        super().add_interface_implementations(interface_uri, implementations_uris)
+        return self
+
+    def add_wrapper(self, wrapper_uri: Uri)-> BaseClientConfigBuilder:
+        super().add_wrapper(wrapper_uri)
+        return self
+
+    def add_wrappers(self, wrappers_uris: List[Uri])-> BaseClientConfigBuilder:
+        super().add_wrappers(wrappers_uris)
+        return self
+
+    def remove_wrapper(self, wrapper_uri: Uri)-> BaseClientConfigBuilder:
+        super().remove_wrapper(wrapper_uri)
+        return self 
+    
+    def set_resolver(self, uri_resolver: IUriResolver)-> BaseClientConfigBuilder:
+        super().set_resolver(uri_resolver)
+        return self
+
+    def add_resolver(self, resolver: IUriResolver)-> BaseClientConfigBuilder:
+        super().add_resolver(resolver)
+        return self
+
+    def add_resolvers(self, resolvers_list: List[IUriResolver])-> BaseClientConfigBuilder:
+        super().add_resolvers(resolvers_list)
+        return self
+
+
