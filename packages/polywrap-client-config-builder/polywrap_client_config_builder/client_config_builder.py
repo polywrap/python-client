39571245f9dc4d72d05c5857be8b5d3505e3936e
DaoAdvocate <12145726+rihp@users.noreply.github.com>
2022-11-25 12:33:08 +0100

    feat:packages property on CCB

--- packages/polywrap-client-config-builder/polywrap_client_config_builder/client_config_builder.py
+++ packages/polywrap-client-config-builder/polywrap_client_config_builder/client_config_builder.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from polywrap_core import Uri, IUriResolver, Env
+from polywrap_core import Uri, IUriResolver, Env, UriWrapper,UriPackage
+from polywrap_uri_resolvers import IUriResolver
 from typing import Dict, Any, List, Optional, Union
 
 @dataclass(slots=True, kw_only=True) 
@@ -11,8 +12,9 @@ class ClientConfig():
     """
     envs: Dict[Uri, Dict[str, Any]]
     interfaces: Dict[Uri, List[Uri]]
+    packages: List[UriPackage]
     resolver: List[IUriResolver]
-    wrappers: List[Uri]
+    wrappers: List[UriWrapper]
 
 
 class BaseClientConfigBuilder(ABC):
@@ -22,7 +24,7 @@ class BaseClientConfigBuilder(ABC):
     """
 
     def __init__(self):
-        self.config = ClientConfig(envs={}, interfaces={}, resolver=[], wrappers= [])
+        self.config = ClientConfig(envs={}, interfaces={}, resolver=[], wrappers= [], packages=[])
 
     @abstractmethod
     def build(self) -> ClientConfig:
@@ -37,13 +39,11 @@ class BaseClientConfigBuilder(ABC):
     @abstractmethod    
     def set_env(self, env: Env, uri: Uri):
         """Sets the envs dictionary in the builder's config, overiding any existing values."""
-        if (env or uri) is None:
-            raise KeyError("Must provide both an env or uri")
         self.config.envs[uri] = env
         return self
 
     @abstractmethod
-    def add_env(self, env: Env = None , uri: Uri = None):
+    def add_env(self, env: Env, uri: Uri):
         """Adds an environment (in the form of an `Env`) for a given uri, without overwriting existing environments,
         unless the env key already exists in the environment, then it will overwrite the existing value"""
         if (env or uri) is None:
@@ -101,6 +101,31 @@ class BaseClientConfigBuilder(ABC):
         Removes a wrapper from the list of wrappers
         """
         self.config.wrappers.remove(wrapper_uri)
+        return self
+
+    @abstractmethod
+    def set_package(self, uri_package: UriPackage):
+        """
+        Sets the package in the builder's config, overiding any existing values.
+        """
+        self.config.packages = [uri_package]
+        return self
+
+    @abstractmethod
+    def add_package(self, uri_package: UriPackage):
+        """
+        Adds a package to the list of packages
+        """
+        self.config.packages.append(uri_package)
+        return self
+    
+    @abstractmethod
+    def add_packages(self, uri_packages: List[UriPackage]):
+        """
+        Adds a list of packages to the list of packages
+        """
+        for uri_package in uri_packages:
+            self.add_package(uri_package)
         return self
 
     @abstractmethod
@@ -170,6 +195,18 @@ class ClientConfigBuilder(BaseClientConfigBuilder):
         super().remove_wrapper(wrapper_uri)
         return self 
     
+    def set_package(self, uri_package: UriPackage)-> BaseClientConfigBuilder:
+        super().set_package(uri_package)
+        return self
+
+    def add_package(self, uri_package: UriPackage)-> BaseClientConfigBuilder:
+        super().add_package(uri_package)
+        return self
+
+    def add_packages(self, uri_packages: List[UriPackage])-> BaseClientConfigBuilder:
+        super().add_packages(uri_packages)
+        return self
+
     def set_resolver(self, uri_resolver: IUriResolver)-> BaseClientConfigBuilder:
         super().set_resolver(uri_resolver)
         return self
