40c497137a4c3effcf6b1893e67aba488b6e3ef3
Niraj Kamdar <nirajkamdar27@pm.me>
2023-03-03 12:04:12 +0400

    fix: publishPackages script

---
+++ scripts/publishPackages.sh
@@ -0,0 +1,303 @@
+function patchVersion() {
+  local package=$1
+  local version=$2
+  if [ -z "$3" ]; then
+    local deps=()
+  else
+    local depsArr=$3[@]
+    local deps=("${!depsArr}")
+  fi
+
+  echo "deps: ${deps[@]}"
+  
+  local pwd=$(echo $PWD)
+
+  cd packages/$package
+
+  poetry version $version
+  local bumpVersionResult=$?
+  if [ $bumpVersionResult -ne 0 ]; then
+    echo "Failed to bump version of $package to $version"
+    exit 1
+  fi
+
+  if [ "${#deps[@]}" -ne "0" ]; then
+    if [ "${deps[0]}" != "" ]; then
+      for dep in "${deps[@]}"; do
+        poetry add $dep@$version
+        local addDepResult=$?
+        if [ $addDepResult -ne 0 ]; then
+          echo "Failed to add $dep@$version to $package"
+          exit 1
+        fi
+      done
+    fi
+  fi
+
+  poetry lock
+  local lockResult=$?
+  if [ $lockResult -ne 0 ]; then
+    echo "Failed to lock $package"
+    exit 1
+  fi
+
+  poetry install
+  local installResult=$?
+  if [ $installResult -ne 0 ]; then
+    echo "Failed to install $package"
+    exit 1
+  fi
+
+  cd $pwd
+}
+
+function publishPackage() {
+  local package=$1
+  local username=$2
+  local password=$3
+
+  local pwd=$(echo $PWD)
+
+  cd packages/$package
+
+  poetry publish --build --username $username --password $password
+  local publishResult=$?
+  if [ $publishResult -ne 0 ]; then
+    echo "Failed to publish $package"
+    exit 1
+  fi
+
+  cd $pwd
+}
+
+function waitForPackagePublish() {
+  local package=$1
+  local version=$2
+  local seconds=0
+
+  while [ $seconds -lt 600 ] # Wait for 10 minutes
+  do
+    poetry search $package | grep "$package \($version\)"
+    local exit_code=$?
+
+    if [ $exit_code -eq 0 ]; then
+      echo "Package $package with version $version is published"
+      break
+    fi
+    sleep 5
+    seconds=$((seconds+5))
+    echo "Waiting for $seconds seconds for the $package to be published"
+  done
+
+  if [ $seconds -eq 600 ]; then
+    echo "Package $package with version $version is not published"
+    exit 1
+  fi
+}
+
+
+# Patching Verion of polywrap-msgpack
+echo "Patching Version of polywrap-msgpack to $1"
+patchVersion polywrap-msgpack $1
+local patchVersionResult=$?
+if [ $patchVersionResult -ne 0 ]; then
+  echo "Failed to bump version of polywrap-msgpack to $1"
+  exit 1
+fi
+
+echo "Publishing polywrap-msgpack"
+publishPackage polywrap-msgpack $2 $3
+local publishResult=$?
+if [ $publishResult -ne 0 ]; then
+  echo "Failed to publish polywrap-msgpack"
+  exit 1
+fi
+
+echo "Waiting for the package to be published"
+waitForPackagePublish polywrap-msgpack $1
+local waitForPackagePublishResult=$?
+if [ $waitForPackagePublishResult -ne 0 ]; then
+  echo "Failed to publish polywrap-msgpack"
+  exit 1
+fi
+
+# Patching Verion of polywrap-result
+echo "Patching Version of polywrap-result to $1"
+patchVersion polywrap-result $1
+local patchVersionResult=$?
+if [ $patchVersionResult -ne 0 ]; then
+  echo "Failed to bump version of polywrap-result to $1"
+  exit 1
+fi
+
+echo "Publishing polywrap-result"
+publishPackage polywrap-result $2 $3
+local publishResult=$?
+if [ $publishResult -ne 0 ]; then
+  echo "Failed to publish polywrap-result"
+  exit 1
+fi
+
+echo "Waiting for the package to be published"
+waitForPackagePublish polywrap-result $1
+local waitForPackagePublishResult=$?
+if [ $waitForPackagePublishResult -ne 0 ]; then
+  echo "Failed to publish polywrap-result"
+  exit 1
+fi
+
+# Patching Verion of polywrap-manifest
+echo "Patching Version of polywrap-manifest to $1"
+deps=(polywrap-msgpack polywrap-result)
+patchVersion polywrap-manifest $1 deps
+local patchVersionResult=$?
+if [ $patchVersionResult -ne 0 ]; then
+  echo "Failed to bump version of polywrap-manifest to $1"
+  exit 1
+fi
+
+echo "Publishing polywrap-manifest"
+publishPackage polywrap-manifest $2 $3
+local publishResult=$?
+if [ $publishResult -ne 0 ]; then
+  echo "Failed to publish polywrap-manifest"
+  exit 1
+fi
+
+echo "Waiting for the package to be published"
+waitForPackagePublish polywrap-manifest $1
+local waitForPackagePublishResult=$?
+if [ $waitForPackagePublishResult -ne 0 ]; then
+  echo "Failed to publish polywrap-manifest"
+  exit 1
+fi
+
+# Patching Verion of polywrap-core
+echo "Patching Version of polywrap-core to $1"
+deps=(polywrap-result polywrap-manifest)
+patchVersion polywrap-core $1 deps
+local patchVersionResult=$?
+if [ $patchVersionResult -ne 0 ]; then
+  echo "Failed to bump version of polywrap-core to $1"
+  exit 1
+fi
+
+echo "Publishing polywrap-core"
+publishPackage polywrap-core $2 $3
+local publishResult=$?
+if [ $publishResult -ne 0 ]; then
+  echo "Failed to publish polywrap-core"
+  exit 1
+fi
+
+echo "Waiting for the package to be published"
+waitForPackagePublish polywrap-core $1
+local waitForPackagePublishResult=$?
+if [ $waitForPackagePublishResult -ne 0 ]; then
+  echo "Failed to publish polywrap-core"
+  exit 1
+fi
+
+# Patching Verion of polywrap-wasm
+echo "Patching Version of polywrap-wasm to $1"
+deps=(polywrap-msgpack polywrap-result polywrap-manifest polywrap-core)
+patchVersion polywrap-wasm $1 deps
+local patchVersionResult=$?
+if [ $patchVersionResult -ne 0 ]; then
+  echo "Failed to bump version of polywrap-wasm to $1"
+  exit 1
+fi
+
+echo "Publishing polywrap-wasm"
+publishPackage polywrap-wasm $2 $3
+local publishResult=$?
+if [ $publishResult -ne 0 ]; then
+  echo "Failed to publish polywrap-wasm"
+  exit 1
+fi
+
+echo "Waiting for the package to be published"
+waitForPackagePublish polywrap-wasm $1
+local waitForPackagePublishResult=$?
+if [ $waitForPackagePublishResult -ne 0 ]; then
+  echo "Failed to publish polywrap-wasm"
+  exit 1
+fi
+
+# Patching Verion of polywrap-plugin
+echo "Patching Version of polywrap-plugin to $1"
+deps=(polywrap-msgpack polywrap-result polywrap-manifest polywrap-core)
+patchVersion polywrap-plugin $1 deps
+local patchVersionResult=$?
+if [ $patchVersionResult -ne 0 ]; then
+  echo "Failed to bump version of polywrap-plugin to $1"
+  exit 1
+fi
+
+echo "Publishing polywrap-plugin"
+publishPackage polywrap-plugin $2 $3
+local publishResult=$?
+if [ $publishResult -ne 0 ]; then
+  echo "Failed to publish polywrap-plugin"
+  exit 1
+fi
+
+echo "Waiting for the package to be published"
+waitForPackagePublish polywrap-plugin $1
+local waitForPackagePublishResult=$?
+if [ $waitForPackagePublishResult -ne 0 ]; then
+  echo "Failed to publish polywrap-plugin"
+  exit 1
+fi
+
+# Patching Verion of polywrap-uri-resolvers
+echo "Patching Version of polywrap-uri-resolvers to $1"
+deps=(polywrap-result polywrap-wasm polywrap-core)
+patchVersion polywrap-uri-resolvers $1 deps
+local patchVersionResult=$?
+if [ $patchVersionResult -ne 0 ]; then
+  echo "Failed to bump version of polywrap-uri-resolvers to $1"
+  exit 1
+fi
+
+echo "Publishing polywrap-uri-resolvers"
+publishPackage polywrap-uri-resolvers $2 $3
+local publishResult=$?
+if [ $publishResult -ne 0 ]; then
+  echo "Failed to publish polywrap-uri-resolvers"
+  exit 1
+fi
+
+echo "Waiting for the package to be published"
+waitForPackagePublish polywrap-uri-resolvers $1
+local waitForPackagePublishResult=$?
+if [ $waitForPackagePublishResult -ne 0 ]; then
+  echo "Failed to publish polywrap-uri-resolvers"
+  exit 1
+fi
+
+# Patching Verion of polywrap-client
+echo "Patching Version of polywrap-client to $1"
+deps=(polywrap-result polywrap-msgpack polywrap-manifest polywrap-core  polywrap-uri-resolvers)
+patchVersion polywrap-client $1 deps
+local patchVersionResult=$?
+if [ $patchVersionResult -ne 0 ]; then
+  echo "Failed to bump version of polywrap-client to $1"
+  exit 1
+fi
+
+echo "Publishing polywrap-client"
+publishPackage polywrap-client $2 $3
+local publishResult=$?
+if [ $publishResult -ne 0 ]; then
+  echo "Failed to publish polywrap-client"
+  exit 1
+fi
+
+echo "Waiting for the package to be published"
+waitForPackagePublish polywrap-client $1
+local waitForPackagePublishResult=$?
+if [ $waitForPackagePublishResult -ne 0 ]; then
+  echo "Failed to publish polywrap-client"
+  exit 1
+fi
