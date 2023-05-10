b114df7c4115db24cd82cfc65f9c9b465b63f495
Niraj Kamdar <nirajkamdar27@pm.me>
2023-05-10 23:53:09 +0400

    feat: add main script

--- packages/polywrap-test-cases/polywrap_test_cases/__init__.py
+++ packages/polywrap-test-cases/polywrap_test_cases/__init__.py
@@ -1,24 +1,29 @@
 """This package contains the utility functions to fetch the wrappers\
     from the wasm-test-harness repo."""
+import io
 import os
 import zipfile
 from urllib.request import urlopen
 from urllib.error import HTTPError
 
-def get_path_to_test_wrappers():
+
+def get_path_to_test_wrappers() -> str:
     return os.path.join(os.path.dirname(__file__), '..', 'wrappers')
 
-def fetch_from_github(url: str):
+
+def fetch_from_github(url: str) -> bytes:
     response = urlopen(url)
     if response.status != 200:
         raise HTTPError(url, response.status, "Failed to fetch file", response.headers, None)
     return response.read()
 
-def unzip_file(file_buffer, destination):
-    with zipfile.ZipFile(file_buffer, 'r') as zip_ref:
+
+def unzip_file(file_buffer: bytes, destination: str) -> None:
+    with zipfile.ZipFile(io.BytesIO(file_buffer), 'r') as zip_ref:
         zip_ref.extractall(destination)
 
-def fetch_wrappers():
+
+def fetch_wrappers() -> None:
     tag = "0.1.0"
     repo_name = "wasm-test-harness"
     url = f"https://github.com/polywrap/{repo_name}/releases/download/{tag}/wrappers"
@@ -26,6 +31,3 @@ def fetch_wrappers():
     buffer = fetch_from_github(url)
     zip_built_folder = get_path_to_test_wrappers()
     unzip_file(buffer, zip_built_folder)
-
-if __name__ == "__main__":
-    fetch_wrappers()
