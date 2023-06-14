838b6165656962e4dc2c5c3436b0d26cc3bb290e
Niraj Kamdar <51387861+Niraj-Kamdar@users.noreply.github.com>
2023-06-14 20:39:44 +0400

    refactor: polywrap-wasm package (#183)

--- packages/polywrap-wasm/polywrap_wasm/constants.py
+++ packages/polywrap-wasm/polywrap_wasm/constants.py
@@ -1,3 +1,5 @@
 """This module contains the constants used by polywrap-wasm package."""
 WRAP_MANIFEST_PATH = "wrap.info"
 WRAP_MODULE_PATH = "wrap.wasm"
+
+__all__ = ["WRAP_MANIFEST_PATH", "WRAP_MODULE_PATH"]
