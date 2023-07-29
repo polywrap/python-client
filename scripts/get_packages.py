0ac6620c9aff875f43b536b350e7d000d42969ba
Niraj Kamdar <nirajkamdar27@pm.me>
2023-07-29 17:56:32 +0700

    feat: update ci

---
+++ scripts/get_packages.py
@@ -0,0 +1,18 @@
+import json
+import os
+
+def extract_package_paths(workspace_file):
+    with open(workspace_file, 'r') as file:
+        workspace_data = json.load(file)
+
+    return [
+        folder['path']
+        for folder in workspace_data['folders']
+        if folder['name'] not in {'root', 'docs'}
+        and os.path.isfile(os.path.join(folder['path'], 'pyproject.toml'))
+    ]
+
+workspace_file = 'python-monorepo.code-workspace'
+package_paths = extract_package_paths(workspace_file)
+
+print(json.dumps(package_paths))
