eb7344217b80e5e6cf3f58b0e6529121f216ecfa
Niraj Kamdar <nirajkamdar27@pm.me>
2023-08-04 14:12:16 +0800

    fix: link packages

--- scripts/link_packages.py
+++ scripts/link_packages.py
@@ -1,15 +1,20 @@
+import os
 import subprocess
+from typing import Dict
 import tomlkit
+from get_packages import extract_package_paths
+from pathlib import Path
 
 
-def link_dependencies():
+def link_dependencies(packages: Dict[str, str]):
     with open("pyproject.toml", "r") as f:
         pyproject = tomlkit.load(f)
+        curdir = Path.cwd().absolute()
 
         for dep in list(pyproject["tool"]["poetry"]["dependencies"].keys()):
             if dep.startswith("polywrap-"):
                 inline_table = tomlkit.inline_table()
-                inline_table.update({"path": f"../{dep}", "develop": True})
+                inline_table.update({"path": os.path.relpath(packages[dep], curdir), "develop": True})
                 pyproject["tool"]["poetry"]["dependencies"].pop(dep)
                 pyproject["tool"]["poetry"]["dependencies"].add(dep, inline_table)
     
@@ -27,6 +32,9 @@ if __name__ == "__main__":
 
     root_dir = Path(__file__).parent.parent
 
+    package_paths = map(Path, extract_package_paths())
+    packages = {path.name: str(path.absolute()) for path in package_paths}
+
     for package_dir in package_build_order():
         with ChangeDir(str(package_dir)):
-            link_dependencies()
+            link_dependencies(packages)
