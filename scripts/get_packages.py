65d2a9acca48722bfdf4e7705af741ac046ac616
Niraj Kamdar <nirajkamdar27@pm.me>
2023-08-16 02:50:30 +0530

    fix: ci

--- scripts/get_packages.py
+++ scripts/get_packages.py
@@ -8,7 +8,7 @@ def extract_package_paths(workspace_file = 'python-monorepo.code-workspace'):
     return [
         folder['path']
         for folder in workspace_data['folders']
-        if folder['name'] not in {'root', 'docs'}
+        if folder['name'] not in {'root', 'docs', 'examples'}
         and os.path.isfile(os.path.join(folder['path'], 'pyproject.toml'))
     ]
 
